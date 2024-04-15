99338165a995eac99a87c0e36948fae1ce74d212
vaishnavicg <vaishnavicg63@gmail.com>
2024-04-15 15:11:21 +0530

    added about page and its related details and resolved conflicts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Header";
+
 import About from "../components/about";
 
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
         <div className="home-section">
             <Navbar />
             <Hero />
+            
             <About />
         </div>
     );
